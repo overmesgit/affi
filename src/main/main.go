04952b82d5920dde186a828441ca1631d2b521e8
overmes <overmes@gmail.com>
2017-09-24 00:43:48 +0300

    add flags

--- src/main/main.go
+++ src/main/main.go
@@ -1,16 +1,46 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"github.com/go-pg/pg"
 	"server"
 )
 
+var (
+	DBPassword string
+	DBName     string
+	DBUser     string
+	Host       string
+)
+
+func Empty(args ...string) bool {
+	for _, val := range args {
+		if val == "" {
+			return true
+		}
+	}
+	return false
+}
+
 func main() {
+	flag.StringVar(&DBUser, "db_user", "", "postgresql db user")
+	flag.StringVar(&DBPassword, "db_password", "", "postgresql db password")
+	flag.StringVar(&DBName, "db_name", "", "postgresql db name")
+	flag.StringVar(&Host, "host", "", "listening ip and port(127.0.0.1:8000)")
+
+	flag.Parse()
+	if Empty(DBUser, DBPassword, DBName, Host) {
+		fmt.Println(DBUser, DBPassword, DBName, Host)
+		flag.PrintDefaults()
+		panic("params not set")
+	}
+
 	db := pg.Connect(&pg.Options{
-		User: "user", Password: "user", Database: "test",
+		User: DBUser, Password: DBPassword, Database: DBName,
 	})
 
-	myServer := server.NewServer("0.0.0.0:8000", db)
+	myServer := server.NewServer(Host, db)
 	myServer.Start()
 
 }
